ca54a9ce4d3799166a730ed5ad57db7b98d6ab44
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-04-01 03:11:44 -0400

    add log system
    Change-Id: If14f7a20dd3dceffdf916fc3b1d09287da98b4fc

--- server.py
+++ server.py
@@ -2,25 +2,23 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import sys
 import ecdf
 import json
+import logging
 
 ADDR = "localhost"
 PORT = 8000
 
-def log(msg):
-	print msg
-
 class RequestHandler(BaseHTTPRequestHandler):		
 	scores = None
 	html = None
+	logger = None
 	def do_GET(self):
 		self.send_response(200, "OK")
 		self.end_headers()
 		self.wfile.write(RequestHandler.html)
 	def do_POST(self):
-		print "POST"
 		length = int(self.headers['Content-length'])
 		data = json.JSONDecoder().decode(self.rfile.read(length))
-		log(data["request"])
+		RequestHandler.logger.info(data["request"])
 		self.send_response(200, "OK")
 		self.end_headers()
 		self.wfile.write(ecdf.percentile(data["percentile"], RequestHandler.scores))
@@ -37,7 +35,14 @@ def main():
 	RequestHandler.scores = ecdf.loadScores(school_name, sys.argv[3:])
 	with open ("template.html", "r") as template:
 		RequestHandler.html = template.read()
-	
+
+	RequestHandler.logger = logging.getLogger('RequestHandler')
+	hdlr = logging.FileHandler('request_history.log')
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	hdlr.setFormatter(formatter)
+	RequestHandler.logger.addHandler(hdlr) 
+	RequestHandler.logger.setLevel(logging.INFO)
+		
 	httpd = HTTPServer((ADDR, PORT), RequestHandler)
 	try:
 		httpd.serve_forever()
