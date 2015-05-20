457bde05f8739bcc3764dbf5b5d0fc909bfa60b1
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-05-20 16:35:21 -0700

    finish project
    Change-Id: I2b638e83c4da62227026d519525f8c1c62e91db5

--- server.py
+++ server.py
@@ -22,17 +22,11 @@ class RequestHandler(BaseHTTPRequestHandler):
 		self.send_response(200, "OK")
 		self.end_headers()
 		self.wfile.write(ecdf.percentile(data["percentile"], RequestHandler.scores))
-		
-def usage():
-	print "usage:", sys.argv[0], "--school", "SCHOOL_NAME", "INPUT_FILE_1", "[INPUT_FILE_i]*"
 
 def main():
+	(school_name, RequestHandler.scores) = ecdf.parseArguments()
 	# argument parsing: ecdf.py --school "name" input_file1 input_file2 ...
-	if (len(sys.argv) < 4 or sys.argv[1] != "--school"):
-		usage()
-		sys.exit(0)
-	school_name = sys.argv[2]
-	RequestHandler.scores = ecdf.loadScores(school_name, sys.argv[3:])
+		
 	with open ("template.html", "r") as template:
 		RequestHandler.html = template.read()
 
